041f4de79ac9c266863637a183860453a190a688
arijit99999 <132125041+arijit99999@users.noreply.github.com>
2024-05-11 16:44:55 +0530

    env

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages,setup
+setup(
+    name='MarketingCampaignClustring',
+    version='0.0.1',
+    author='arijit dey',
+    author_email='deyarijit063@gmail.com',
+   packages=find_packages()
+)
